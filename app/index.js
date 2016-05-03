ffeea9c8ed2d21c61ad7d2428c8d54d06cd69305
Kathy Truong <kathy.yvy.truong@gmail.com>
2016-05-02 22:08:19 -0500

    fix port

--- app/index.js
+++ app/index.js
@@ -1,12 +1,11 @@
 const express = require('express')
 const app = express()
-const port = 3000
 
 app.get('/', (request, response) => {
     response.send('Hello from Express!')
 })
 
-app.listen(port, (err) => {
+app.listen(process.env.PORT || 3000, (err) => {
     if (err) {
         return console.log('something bad happened', err)
     }
